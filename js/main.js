3c29985d66b93e9efce3baf174a3d4893f34240e
John Aps <johnaps94@gmail.com>
2023-08-17 22:40:39 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -138,4 +138,65 @@ animatePanningImages('mana', 'left', true);
  *   END OF - ADJUST HEALTH FILL CLIPPING
  *
  */
+
+
+document.querySelectorAll('.title').forEach(title => {
+    title.addEventListener('click', function() {
+        const inputValue = this.innerText;
+        const inputElem = document.createElement('input');
+        inputElem.value = inputValue;
+
+        // Function to revert input back to the title
+        const revertToTitle = () => {
+            title.innerText = inputElem.value;
+            if(inputElem.parentNode) {  // Check to ensure that inputElem is still in the DOM
+                inputElem.parentNode.replaceChild(title, inputElem);
+            }
+        }
+
+        // Event Listener for the blur event on the input
+        inputElem.addEventListener('blur', revertToTitle);
+
+        // Event Listener for the Enter key on the input
+        inputElem.addEventListener('keydown', function(e) {
+            if (e.key === 'Enter') {
+                e.preventDefault(); // prevent any default actions associated with the Enter key
+                revertToTitle();
+            }
+        });
+
+        // Replace the title with the input element
+        this.parentNode.replaceChild(inputElem, title);
+        inputElem.focus();
+    });
+});
+
+
+document.querySelectorAll('.title').forEach(title => {
+    title.addEventListener('contextmenu', function(e) {
+        e.preventDefault(); // Prevent the default right-click menu from showing up
+
+        const description = this.nextElementSibling;
+
+        if (description.style.display === 'inline-block') {
+            description.style.display = 'none';
+        } else {
+            // Hide all other descriptions
+            document.querySelectorAll('.description').forEach(desc => desc.style.display = 'none');
+
+            // Show this description
+            description.style.display = 'inline-block';
+        }
+    });
+});
+
+// Event listener to handle hiding the description when clicking elsewhere
+document.addEventListener('click', function(e) {
+    // If the clicked element is not a title and not a description, hide all descriptions
+    if (!e.target.classList.contains('title') && !e.target.classList.contains('description')) {
+        document.querySelectorAll('.description').forEach(desc => desc.style.display = 'none');
+    }
+});
+
+  
 });

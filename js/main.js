9b06a4d190b671a61026829d2c374e4bd831f32b
John Aps <johnaps94@gmail.com>
2023-08-20 22:20:03 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+//function that will stay in main.js and be called in spellbook.js
+function toggleActiveClass(toggleElement, activeElement) {
+    const toggles = document.querySelectorAll(toggleElement);
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', function() {
+            this.closest(activeElement).classList.toggle('active');
+        });
+    });
+}
+toggleActiveClass('.spell-lvl-heading', '.spell-lvl-slots');
+
 /* 
  *
  *   SPELLBOOK FUNCTIONS
@@ -6,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 //spell title element -> swap to input when clicked
-document.querySelectorAll('.title').forEach(title => {
+/* document.querySelectorAll('.title').forEach(title => {
     title.addEventListener('click', function() {
         const inputValue = this.innerText;
         const inputElem = document.createElement('input');
@@ -16,13 +28,7 @@ document.querySelectorAll('.title').forEach(title => {
         const revertToTitle = () => {
             title.innerText = inputElem.value;
             if(inputElem.parentNode) {  // Check to ensure that inputElem is still in the DOM
-                title.classList.add('no-animation');  // Add the no-animation class
                 inputElem.parentNode.replaceChild(title, inputElem);  
-
-                // Remove the no-animation class after a brief timeout
-                setTimeout(() => {
-                    title.classList.remove('no-animation');
-                }, 600);  // 10ms should be enough, but adjust if necessary
             }
         }
 
@@ -60,35 +66,11 @@ document.querySelectorAll('.title').forEach(title => {
             description.style.display = 'inline-block';
         }
     });
-});
-
-// Event listener to handle hiding the description when clicking elsewhere
-document.addEventListener('click', function(e) {
-    // If the clicked element is not a title and not a description, hide all descriptions
-    /* if (!e.target.classList.contains('title') && !e.target.classList.contains('description')) { */
-    if (!e.target.classList.contains('description')) {
-        document.querySelectorAll('.description').forEach(desc => desc.style.display = 'none');
-    }
-});
+}); */
 
 
 
-
-
-//function that will stay in main.js and be called in spellbook.js
-function toggleActiveClass(toggleElement, activeElement) {
-    const toggles = document.querySelectorAll(toggleElement);
-    toggles.forEach(toggle => {
-        toggle.addEventListener('click', function() {
-            this.closest(activeElement).classList.toggle('active');
-        });
-    });
-}
-toggleActiveClass('.spell-lvl-heading', '.spell-lvl-slots');
-
-
-
-function transitionDelayOnSpells() {
+function transitionDelayOnSpells(delayIncrement) {
     // Grab all the .title elements inside .spell
     let spells = document.querySelectorAll('.spell-lvl-slots .spell');
 
@@ -102,15 +84,106 @@ function transitionDelayOnSpells() {
         // Dynamically set translateY value based on the title's height
         title.style.transform = `translateY(-${title.offsetHeight}px)`;
 
-        /* // Increment delay for the transition effect
-        title.style.transitionDelay = `${delay}s`; */
-
         // Set the animation delay
         title.style.animationDelay = `${delay}s`;
 
-        delay += 0.065; // increment delay by 0.2s for each title
+        delay += delayIncrement; // increment delay by delayIncrement seconds for each title
     });
 }
-transitionDelayOnSpells();
+transitionDelayOnSpells(0.065);
+
+function SingleAndDoubleClick(element, helperFunc) {
+    let allElementsAlike = document.querySelectorAll(element);
+    let clickTimeout = null;  // This will store the setTimeout reference
+
+    allElementsAlike.forEach(ele => {
+        ele.addEventListener('click', function(e) {
+            if (clickTimeout === null) {
+                // First click
+                clickTimeout = setTimeout(() => {
+                    // Single click action
+                    clickTimeout = null;
+                    // TODO: Insert your single click logic here
+                    helperFunc(this);
+                }, 300); // 300ms to wait for a potential second click
+            } else {
+                // Second click detected before timeout completed
+                clearTimeout(clickTimeout); // clear the timeout
+                clickTimeout = null;
+                
+                // Logic for edit functionality
+                const inputValue = this.innerText;
+                const inputElem = document.createElement('textarea');
+
+                /* inputElem.classList.add(cssClass); */
+                inputElem.value = inputValue;
+
+                // Function to revert input back to the title
+                const revertToTitle = () => {
+                    ele.innerText = inputElem.value;
+                    if (inputElem.parentNode) {
+                        inputElem.parentNode.replaceChild(ele, inputElem);
+                    }
+                }
+                const x = ele.scrollHeight;
+                /* inputElem.addEventListener('blur', revertToTitle); */
+                inputElem.addEventListener('keydown', function(e) {
+                    if (e.key === 'Enter') {
+                        e.preventDefault();
+                        revertToTitle();
+                    }
+                });
+
+                this.parentNode.replaceChild(inputElem, ele);
+                inputElem.focus();
+
+                inputElem.addEventListener('input', function() {
+                    this.style.height = 'auto';  // Reset the height
+                    this.style.height = this.scrollHeight + 'px';  // Set it to its scroll height
+                });
+
+                // Initial adjust if there's default text
+                inputElem.style.height = `${x-1}px`;
+
+            }
+        });
+    });
+}
+// Event listener to handle hiding the description when clicking elsewhere
+/* document.addEventListener('click', function(e) { */
+    // If the clicked element is not a title and not a description, hide all descriptions
+    /* if (!e.target.classList.contains('title') && !e.target.classList.contains('description')) { */
+    /* if (!e.target.classList.contains('description')) {
+        document.querySelectorAll('.description').forEach(desc => desc.style.display = 'none');
+    }
+}); */
+SingleAndDoubleClick('.spell-lvl-slots .spell .title', FirstClickShowDescriptionHelper);
+SingleAndDoubleClick('.spell-lvl-slots .spell .description span', blank);
+
+
+function blank(thiS) {
+    return;
+}
+
+let activeSpellOneClick;
+function FirstClickShowDescriptionHelper(thiS) {
+
+    if (activeSpellOneClick && thiS !== activeSpellOneClick) {
+        activeSpellOneClick.closest('.description').style.display = 'none';
+    }
+    const description = thiS.nextElementSibling;
+    activeSpellOneClick = thiS;
+    if (description.style.display === 'inline-block') {
+        description.style.display = 'none';
+    } else {
+        // Hide all other descriptions
+        document.querySelectorAll('.description').forEach(desc => desc.style.display = 'none');
+
+        // Show this description
+        description.style.display = 'inline-block';
+    }
+}
+
+
 
 });

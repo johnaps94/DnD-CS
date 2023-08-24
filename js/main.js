ac3aa7888ec2ca733c032145e4d883cf6dc8a9e1
John Aps <johnaps94@gmail.com>
2023-08-25 02:42:52 +0300

    eddd

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { adjustTextareaHeight, transitionDelayOnSpells } from '/js/spellbook.js'
 document.addEventListener("DOMContentLoaded", function() {
 
     //function that will stay in main.js and be called in spellbook.js
+    //incorporate this inside the clickhandler class as the first click action of heading element
     function toggleActiveClass(toggleElement, activeElement) {
         const toggles = document.querySelectorAll(toggleElement);
         toggles.forEach(toggle => {
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
      *   User Profile
      *
      */
-    const userProfile = {
+    /* const userProfile = {
         'name': characherName,
         'health': currentHealth,
         'skills': skillsObj,
@@ -28,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
         constructor() {
             this.spellName = this.querySelector(); 
         }
-    }
+    } */
 
 
     /* 
@@ -58,10 +59,11 @@ document.addEventListener("DOMContentLoaded", function() {
             });
 
             // Add an event listener to the document to handle outside clicks
+            // maybe make a logical check to check if the following will be run
             document.addEventListener('click', (e) => {
                 const isClickInside = Array.from(this.elements).some(ele => ele.contains(e.target));
                 if (!isClickInside && this.clickOutsideCallback) {
-                    this.clickOutsideCallback();
+                    this.clickOutsideCallback(e);
                 }
             });
         }
@@ -81,8 +83,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         handleDoubleClick(ele) {
             if (this.clickOutsideCallback) {
-                this.clickOutsideCallback();
-            }
+                this.clickOutsideCallback();// check this in the ai conv
+            } 
             const inputValue = ele.innerText.trim();
             const inputElem = document.createElement(this.doubleClickElementType);
 
@@ -110,14 +112,29 @@ document.addEventListener("DOMContentLoaded", function() {
             this.doubleClickHelperFunc && this.doubleClickHelperFunc(inputElem);
         }
     }
-    new ClickHandler('.spell-lvl-slots .spell .title', toggleDescription, 'input', 'ta-title', null, () => {
-        if (openedDescription) {
+    new ClickHandler('.spell-lvl-slots .spell .title', toggleDescription, 'input', 'spell-edit-title', null, (e) => {
+        // Check if the event target is not inside the openedDescription
+        if (openedDescription && !openedDescription.contains(e.target)) {
             openedDescription.style.display = 'none';
             openedDescription = null;
         }
     });
-    new ClickHandler('.spell-lvl-slots .spell .description span', null, 'textarea', 'ta-description', adjustTextareaHeight);
+    new ClickHandler('.spell-lvl-slots .spell .description span', null, 'textarea', 'spell-edit-description', adjustTextareaHeight);
+    new ClickHandler('.spell-lvl-slots .spell-lvl-heading h2 span', null, 'input', 'spell-edit-heading', spellLevelInputWidthAdjust, null);
 
+    function spellLevelInputWidthAdjust(inputElem) {
+        console.log("Function called!");
+        console.log(inputElem);
+        const adjustWidth = () => {
+            inputElem.style.width = `${inputElem.scrollWidth}px`;
+        };
+    
+        // Adjust width when the function is called
+        adjustWidth();
+    
+        // Adjust width on input changes
+        inputElem.addEventListener('input', adjustWidth);
+    }
     function toggleDescription(ele) {
         const description = ele.nextElementSibling;
         // Close the previously opened description
